1d6ee19a9b808831734971fc6ab4ce5576ebbeca
MicahAngeles <micah27@hawaii.edu>
2014-11-05 12:46:52 -1000

    add a couple of functions
    added a couple of functions

--- app/js/algo/padi.js
+++ app/js/algo/padi.js
@@ -24,6 +24,7 @@ padi.DEFAULT_SURFACE_INTERVAL = 60;
 var time = null; 
 var depth = null;
 var pressureGroup = null; 
+var rnt = null; 
 
 padi.badDive  = function(time, deptth) {
 	var magicnumB = 105.4459074484564; 
@@ -58,13 +59,39 @@ padi.warningDive = function(time, depth) {
 
 //TODO 
 padi.pressureGroup = function(time, depth, pressureGroup) {
+	//getting the pressure group?
+	var s1 = (2.2648834727001601*Math.pow(10, 160)); 
+	var m1 = 7.0123592040257003; 
+	var n1 = 1.7946238745730789; 
+	var q1 = 552.85426276703538; 
+	var r1 = −20.363335715433173; 
+	var c1 = −1.0231048129283549; 
+
 	return pressureGroup; 
-}
+};
 
 //TODO 
 padi.reducePressureGroup = function() {
-	return pressureGroup; 
-}
+	//reducing pressure group 
 
+	var s2 = 116.54299853808371; 
+	var m2 = 33.212036458693376; 
+	var n2 = −15.10250855396535; 
+	var q2 = −186.32853553152427; 
+	var r2 = 112.18008663409428; 
+	var c2 = 0.82154053080283274; 
+	return pressureGroup; 
+};
+
+//TODO 
+padi.rnt = function() {
+	var c3 = 76.081117597706665; 
+	var m3 = 4.1581576427587992; 
+	var n3 = −19.592050053069073; 
+	var q3 = −0.5708514716494717; 
+	var r3 = 0.46114751660456582; 
+	return rnt; 
+
+};
 module.exports = padi;
 
